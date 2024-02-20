0e2fbcf8e4180180b087d1044246d6b8c48d701f
Stephen Ehlert <ehlert.stephenj@gmail.com>
2024-02-20 07:55:07 -0700

    lab 4 initial 2

---
+++ lab_4/MDK-ARM/lab_4/stm32f0xx_hal.d
@@ -0,0 +1,31 @@
+lab_4/stm32f0xx_hal.o: \
+  C:\Users\ehler\STM32Cube\Repository\STM32Cube_FW_F0_V1.11.4\Drivers\STM32F0xx_HAL_Driver\Src\stm32f0xx_hal.c \
+  C:\Users\ehler\STM32Cube\Repository\STM32Cube_FW_F0_V1.11.4\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal.h \
+  ..\Core\Inc\stm32f0xx_hal_conf.h \
+  C:\Users\ehler\STM32Cube\Repository\STM32Cube_FW_F0_V1.11.4\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_rcc.h \
+  C:\Users\ehler\STM32Cube\Repository\STM32Cube_FW_F0_V1.11.4\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_def.h \
+  C:\Users\ehler\STM32Cube\Repository\STM32Cube_FW_F0_V1.11.4\Drivers\CMSIS\Device\ST\STM32F0xx\Include\stm32f0xx.h \
+  C:\Users\ehler\STM32Cube\Repository\STM32Cube_FW_F0_V1.11.4\Drivers\CMSIS\Device\ST\STM32F0xx\Include\stm32f072xb.h \
+  C:\Users\ehler\STM32Cube\Repository\STM32Cube_FW_F0_V1.11.4\Drivers\CMSIS\Include\core_cm0.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\stdint.h \
+  C:\Users\ehler\STM32Cube\Repository\STM32Cube_FW_F0_V1.11.4\Drivers\CMSIS\Include\cmsis_version.h \
+  C:\Users\ehler\STM32Cube\Repository\STM32Cube_FW_F0_V1.11.4\Drivers\CMSIS\Include\cmsis_compiler.h \
+  C:\Users\ehler\STM32Cube\Repository\STM32Cube_FW_F0_V1.11.4\Drivers\CMSIS\Include\cmsis_armclang.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\arm_compat.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\arm_acle.h \
+  C:\Users\ehler\STM32Cube\Repository\STM32Cube_FW_F0_V1.11.4\Drivers\CMSIS\Device\ST\STM32F0xx\Include\system_stm32f0xx.h \
+  C:\Users\ehler\STM32Cube\Repository\STM32Cube_FW_F0_V1.11.4\Drivers\STM32F0xx_HAL_Driver\Inc\Legacy\stm32_hal_legacy.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\stddef.h \
+  C:\Users\ehler\STM32Cube\Repository\STM32Cube_FW_F0_V1.11.4\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_rcc_ex.h \
+  C:\Users\ehler\STM32Cube\Repository\STM32Cube_FW_F0_V1.11.4\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_gpio.h \
+  C:\Users\ehler\STM32Cube\Repository\STM32Cube_FW_F0_V1.11.4\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_gpio_ex.h \
+  C:\Users\ehler\STM32Cube\Repository\STM32Cube_FW_F0_V1.11.4\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_exti.h \
+  C:\Users\ehler\STM32Cube\Repository\STM32Cube_FW_F0_V1.11.4\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_dma.h \
+  C:\Users\ehler\STM32Cube\Repository\STM32Cube_FW_F0_V1.11.4\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_dma_ex.h \
+  C:\Users\ehler\STM32Cube\Repository\STM32Cube_FW_F0_V1.11.4\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_cortex.h \
+  C:\Users\ehler\STM32Cube\Repository\STM32Cube_FW_F0_V1.11.4\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_flash.h \
+  C:\Users\ehler\STM32Cube\Repository\STM32Cube_FW_F0_V1.11.4\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_flash_ex.h \
+  C:\Users\ehler\STM32Cube\Repository\STM32Cube_FW_F0_V1.11.4\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_i2c.h \
+  C:\Users\ehler\STM32Cube\Repository\STM32Cube_FW_F0_V1.11.4\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_i2c_ex.h \
+  C:\Users\ehler\STM32Cube\Repository\STM32Cube_FW_F0_V1.11.4\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_pwr.h \
+  C:\Users\ehler\STM32Cube\Repository\STM32Cube_FW_F0_V1.11.4\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_pwr_ex.h
