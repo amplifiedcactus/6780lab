7d912464847838615e999c6862abeda6b1a4db69
Stephen Ehlert <ehlert.stephenj@gmail.com>
2024-02-20 10:09:47 -0700

    lab 4 checkoff 1 complete

--- lab_4/Core/Src/main.c
+++ lab_4/Core/Src/main.c
@@ -53,7 +53,31 @@ void SystemClock_Config(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+void sctransmit(char x) {
+	
+	//wait for transmit register to be empty
+	while (1) {
+		if (USART3->ISR & (1 << 7)) {
+			break;
+		}
+	}
+	
+	//write character to transmit register
+	USART3->TDR = x;
+}
 
+void string_transmit (char a[]) {
+	int counter = 0;
+	while (1) {
+		if (a[counter] != 0) {
+			sctransmit(a[counter]);
+			counter = counter + 1;
+		}
+		else
+			break;
+	}
+	
+}
 /* USER CODE END 0 */
 
 /**
@@ -69,33 +93,86 @@ int main(void)
   /* MCU Configuration--------------------------------------------------------*/
 
   /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+  
+	HAL_Init(); // Reset of all peripherals, init the Flash and Systick
+	SystemClock_Config(); //Configure the system clock
+	
+	RCC->AHBENR |= RCC_AHBENR_GPIOCEN; // Enable peripheral clock to GPIOC
+	
+	
+	RCC->AHBENR |= RCC_AHBENR_GPIOBEN; // Enable peripheral clock to GPIOB
+	RCC->APB1ENR |= RCC_APB1ENR_USART3EN; // Enable peripheral clock to USART3
+	
+	
+	
+	// Initialize pins PC6 (red), PC7 (blue), PC8 (orange), PC9 (green)
+	// Set to general purpose output mode
+	GPIOC->MODER |= (1 << 18) | (1 << 16) | (1 << 14) | (1 << 12);
+	GPIOC->MODER &= ~((1 << 19) |(1 << 17) |(1 << 15) | (1 << 13));
+	// Set to push pull output type
+	GPIOC->OTYPER &= ~(0x000FF000); //0000 0000 0000 1111 1111 0000 0000 0000
+	// Set to low speed
+	GPIOC->OSPEEDR &= ~(0x000FF000);
+	// Set to no pullup/down resistor
+	GPIOC->PUPDR &= ~(0x000FF000);
+	
+	// Set PB10 and PB11 to alternate function mode
+	GPIOB->MODER |= (1 << 23) | (1 << 21);
+	GPIOB->MODER &= ~((1 << 22) | (1 << 20));
+	// Set to push pull output type
+	GPIOB->OTYPER &= ~((1 << 11) | (1 << 10));
+	// Set to low speed
+	GPIOB->OSPEEDR &= ~((1 << 23) | (1 << 22) | (1 << 21) | (1 << 20));
+	// Set to no pullup/down resistor
+	GPIOB->PUPDR &= ~((1 << 23) | (1 << 22) | (1 << 21) | (1 << 20));
+	
+	
+	
+	//Select AF4 for PB10 and PB11
+	GPIOB->AFR[1] |= 0x04 << GPIO_AFRH_AFSEL10_Pos;
+	GPIOB->AFR[1] |= 0x04 << GPIO_AFRH_AFSEL11_Pos;
+	
+	//set baud rate to about 115200
+	USART3->BRR |= HAL_RCC_GetHCLKFreq() / 115200;
+	
+	//HAL_RCC_GetHCLKFreq() / 115200 = 69.44444
+	
+	//enable transmitter and receiver
+	USART3->CR1 |= (1 << 3) | (1 << 2);
+	
+	//enable USART 3
+	USART3->CR1 |= (1 << 0);
+	
 
-  /* USER CODE BEGIN Init */
+	
+	//GPIOC->ODR &= ~(1 << 8);
+	char name[] = {'S', 't', 'e', 'p', 'h', 'e', 'n', 0};
+	char error[] = {'e', 'r', 'r', 'o', 'r', '\n', 0};
+	char c = 0;
+	//char read = 0;
+	
+	while (1) {
+		//string_transmit(name);
+		//HAL_Delay(200); // Delay 2000ms
+		
+		//if read register is not empty, read data
+		if (USART3->ISR & (1 << 5)){
+			c = USART3->RDR;
+			
+			if (c == 'g')
+				GPIOC->ODR ^= (1 << 9);
+			else if (c == 'r')
+				GPIOC->ODR ^= (1 << 6);
+			else if (c == 'b')
+				GPIOC->ODR ^= (1 << 7);
+			else if (c == 'o')
+				GPIOC->ODR ^= (1 << 8);
+			else
+				string_transmit(error);
+		}
+		
+	}
 
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
-  SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
-  /* USER CODE BEGIN 2 */
-
-  /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
-  }
-  /* USER CODE END 3 */
 }
 
 /**
